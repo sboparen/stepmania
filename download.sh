d4abe1ca8137cfd8e70be29cff8bf5b4ebf9ed94
Simon Parent <sboparen@gmail.com>
2017-01-04 20:47:50 -0500

    Install Simply Love theme.
    https://github.com/dguzek/Simply-Love-SM5

--- download.sh
+++ download.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -e
-cd "$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
-cd NoteSkins/dance
+d="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
+# Download Cel noteskin.
+cd "$d"/NoteSkins/dance
 if ! test -e USWCelSM5; then
     rm -f USWCelSM5.rar
     wget http://zettbou.co.uk/Unlimited_Stepman_Works/_Noteskin/SM5/Dance/USWCelSM5.rar
     sha1sum --strict --check USWCelSM5.sha1sum
     unrar x USWCelSM5.rar
+fi
+# Download Simply Love theme.
+cd "$d"/Themes
+if ! test -e 'Simply Love'; then
+    rm -f Simply.Love.v3.zip
+    wget https://github.com/dguzek/Simply-Love-SM5/releases/download/3.0/Simply.Love.v3.zip
+    sha1sum --strict --check Simply.Love.v3.sha1sum
+    unzip Simply.Love.v3.zip
 fi
