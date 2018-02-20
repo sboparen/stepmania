caf130c709c83a01cfb36e85effa25026d143e7b
Simon Parent <sboparen@gmail.com>
2018-02-20 09:28:16 -0500

    Cel noteskin.

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 cd "$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
+# Download cel noteskin if needed.
+(
+    cd NoteSkins/dance
+    if ! test -e USWCelSM5; then
+	rm -f USWCelSM5.rar
+	wget http://zettbou.co.uk/Unlimited_Stepman_Works/_Noteskin/SM5/Dance/USWCelSM5.rar
+	sha1sum --strict --check USWCelSM5.sha1sum
+	unrar x USWCelSM5.rar
+    fi
+)
+# Build stepmania.
 cd Build
 cmake -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE=Release -DWITH_MINIMAID=OFF ..
 cmake ..
